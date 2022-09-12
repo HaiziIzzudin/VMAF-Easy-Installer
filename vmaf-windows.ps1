917d5f41ff11c7e0d9e681eaafbf42cffaded2f5
Haizi? <79714350+HaiziIzzudin@users.noreply.github.com>
2022-09-12 21:10:53 +0800

    windows build

---
+++ vmaf-windows.ps1
@@ -0,0 +1,20 @@
+# prerequisites software install
+Set-Location %USERPROFILE%\AppData\Local\Microsoft\WindowsApps;
+
+winget install Git.Git;
+winget install Rustlang.Rustup;
+winget install Kitware.CMake;
+winget install Microsoft.VisualStudio.2022.Community;
+Invoke-WebRequest -Uri http://www.tortall.net/projects/yasm/releases/yasm-1.3.0-win64.exe -OutFile .\yasm.exe;
+
+Invoke-WebRequest -Uri https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip -OutFile .\ffmpeg.zip;
+Expand-Archive -LiteralPath ".\ffmpeg.zip" -DestinationPath ".\";
+Remove-Item ".\ffmpeg.zip" -Recurse -Force;
+Move-Item -Path .\ffmpeg-master-latest-win64-gpl\bin\ffmpeg.exe -Destination .\; # Move ffmpeg.exe to userdata
+Move-Item -Path .\ffmpeg-master-latest-win64-gpl\bin\ffprobe.exe -Destination .\; # Move ffmpeg.exe to userdata
+Remove-Item ".\ffmpeg-master-latest-win64-gpl\" -Recurse -Force;
+
+cargo install ab-av1; # install ab-av1
+
+# preparation of SvtAv1EncApp
+git clone https://gitlab.com/AOMediaCodec/SVT-AV1.git;
