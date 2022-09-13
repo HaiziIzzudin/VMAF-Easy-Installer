2e1aa0d9b2aa4a96bb492ffd827faf9fbba769cd
Haizi? <79714350+HaiziIzzudin@users.noreply.github.com>
2022-09-14 00:47:14 +0800

    ddd

--- vmafeasy-v3.ps1
+++ vmafeasy-v3.ps1
@@ -56,12 +56,13 @@ if ($platform -eq "WINDOWS") {
 else { # IF PLATFORM = LINUX
     if ((Test-Path -Path "~/.vmafav1softwareidentifier") -eq $false) {
         Read-Host -Prompt "PREREQUISITES IS NOT INSTALLED! Press [ENTER] to begin installation"; Clear-Host;
+        sudo apt install software-properties-common -y;  # NOT INSTALLED BY DEFAULT IN DEBIAN, INSTALLED BY DEFAULT IN UBUNTU
         sudo add-apt-repository ppa:savoury1/ffmpeg4 -y; # ADDING PPA:SAVOURY FFMPEG 4
         sudo add-apt-repository ppa:savoury1/ffmpeg5 -y; # ADDING PPA:SAVOURY FFMPEG 5
         curl https://sh.rustup.rs -sSf | sh;             # INSTALL RUSTUP CARGO
         touch ~/.vmafav1softwareidentifier;              # SOFTWARE IDENTIFIER (DO NOT DELETE)
     }
-    sudo apt update; sudo apt upgrade -y; sudo apt install ffmpeg gcc cmake yasm clang-11 -y;
+    sudo apt update; sudo apt upgrade -y; sudo apt install ffmpeg gcc cmake yasm clang -y;
     
     Write-Host "`nPREREQUISITES INSTALL HAS COMPLETED!`n`n"; Pause; Clear-Host;
     
