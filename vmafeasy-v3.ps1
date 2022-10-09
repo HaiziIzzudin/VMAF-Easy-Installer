9a609073323f71bb244bacd3dfd99319a7f8d4dc
Haizi? <79714350+HaiziIzzudin@users.noreply.github.com>
2022-10-09 17:27:50 +0800

    Modify windows

--- vmafeasy-v3.ps1
+++ vmafeasy-v3.ps1
@@ -7,48 +7,29 @@ else { $platform = "LINUX"; }
 Clear-Host; Write-Host "VMAF SUPER EASY INSTALLER FEAT. SVT-AV1 - YOUR PLATFORM IS $platform`nThis program will help you install and get VMAF up and running with minimal clicks and interactions.`n`nLet's check prerequisites first...`n`n";
 
 if ($platform -eq "WINDOWS") {
-    if (((Test-Path -Path "C:\Program Files\Git\bin\git.exe")                -and (Test-Path -Path "~\.cargo\bin\cargo.exe")                            -and 
-        (Test-Path -Path "C:\Program Files\CMake\bin\cmake.exe")             -and (Test-Path -Path "~\AppData\Local\Microsoft\WindowsApps\yasm.exe")    -and
-        (Test-Path -Path "~\AppData\Local\Microsoft\WindowsApps\ffmpeg.exe") -and (Test-Path -Path "~\AppData\Local\Microsoft\WindowsApps\ffprobe.exe") -and
-        (Test-Path -Path "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe")) -eq $false) {
+    if (((Test-Path -Path "~\.cargo\bin\cargo.exe") -and (Test-Path -Path "~\AppData\Local\Microsoft\WindowsApps\ffmpeg.exe") -and (Test-Path -Path "~\AppData\Local\Microsoft\WindowsApps\ffprobe.exe")) -eq $false) {
     
         Read-Host -Prompt "PREREQUISITES IS NOT INSTALLED! Press [ENTER] to begin installation"; Clear-Host;
     
         Set-Location ~\AppData\Local\Microsoft\WindowsApps; # CD TO PATH
 
-        Invoke-WebRequest -Uri http://www.tortall.net/projects/yasm/releases/yasm-1.3.0-win64.exe -OutFile .\yasm.exe; # YASM INSTALL
         Invoke-WebRequest -Uri https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip -OutFile .\ffmpeg.zip;
-        Expand-Archive -LiteralPath ".\ffmpeg.zip" -DestinationPath ".\";                                              # DOWNLOAD & EXTRACT FFMPEG
-        Move-Item -Path .\ffmpeg-master-latest-win64-gpl\bin\ffmpeg.exe -Destination .\;                               # TAKEOUT FFMPEG.EXE TO PATH
-        Move-Item -Path .\ffmpeg-master-latest-win64-gpl\bin\ffprobe.exe -Destination .\;                              # TAKEOUT FFPROBE.EXE TO PATH
-        Remove-Item ".\ffmpeg.zip" -Recurse -Force;                                                                    # REMOVE FFMPEG ZIP
-        Remove-Item ".\ffmpeg-master-latest-win64-gpl\" -Recurse -Force;                                               # REMOVE FFMPEG FOLDER
-        Invoke-WebRequest -Uri "https://c2rsetup.officeapps.live.com/c2r/downloadVS.aspx?sku=community&channel=Release&version=VS2022&source=VSLandingPage&includeRecommended=true&cid=2030:63428752a0eb4551b273af591d9c7491" -OutFile .\msvc.exe; .\msvc.exe;            # DOWNLOAD & RUN MSVC MANUALLY
-        Clear-Host; Write-Host "A Visual-Studio-Installer installer has running.`n`nOn WORKLOADS tab, tick [Desktop development with C++], and click [INSTALL].`n`nAfter installation, you need to run it for first time.`n`nAfter running, you can close it."; Pause;
-        Remove-Item ".\msvc.exe" -Recurse -Force;                                                                      # REMOVE MSVC-INSTALLER INSTALLER
-
-        winget install Git.Git; winget install Rustlang.Rustup; winget install Kitware.CMake;                          # WINGETS
+        Expand-Archive -LiteralPath ".\ffmpeg.zip" -DestinationPath ".\";                 # DOWNLOAD & EXTRACT FFMPEG
+        Move-Item -Path .\ffmpeg-master-latest-win64-gpl\bin\ffmpeg.exe -Destination .\;  # TAKEOUT FFMPEG.EXE TO PATH
+        Move-Item -Path .\ffmpeg-master-latest-win64-gpl\bin\ffprobe.exe -Destination .\; # TAKEOUT FFPROBE.EXE TO PATH
+        Remove-Item ".\ffmpeg.zip" -Recurse -Force;                                       # REMOVE FFMPEG ZIP
+        Remove-Item ".\ffmpeg-master-latest-win64-gpl\" -Recurse -Force;                  # REMOVE FFMPEG-MASTER FOLDER
+        winget install Rustlang.Rustup;                                                   # WINGETS
 
         # GIVING INSTRUCTIONS TO USER
         Clear-Host; Write-Host "Installation has completed. A reboot is required for be able to run this program."; Pause; exit; # EXIT WT (BCOS WINDOWS RESTART IS REQUIRED)
         }
     else {
         Write-Host "PREREQUISITES IS COMPLETE!"; Pause;
-    
-        Set-Location ~\AppData\Local\Microsoft\WindowsApps; # PATHING LOCATIONS
 
-        # PREPARATION OF SVTAV1ENCAPP BUILDER
-        git clone https://gitlab.com/AOMediaCodec/SVT-AV1.git ;                                                     # CLONE SVTAV1 REPO
-        ~\AppData\Local\Microsoft\WindowsApps\SVT-AV1\Build\windows\build.bat 2022 release                          # START BUILD SVTAV1ENCAPP
-        Move-Item -Path ~\AppData\Local\Microsoft\WindowsApps\SVT-AV1\Bin\Release\SvtAv1EncApp.exe -Destination .\; # TAKEOUT SVTAV1ENCAPP.EXE TO PATH 
-        Move-Item -Path ~\AppData\Local\Microsoft\WindowsApps\SVT-AV1\Bin\Release\SvtAv1Enc.dll -Destination .\;    # TAKEOUT SVTAV1ENC.DLL TO PATH
-        Move-Item -Path ~\AppData\Local\Microsoft\WindowsApps\SVT-AV1\Bin\Release\SvtAv1Enc.exp -Destination .\;    # TAKEOUT SVTAV1ENC.EXP TO PATH
-        Move-Item -Path ~\AppData\Local\Microsoft\WindowsApps\SVT-AV1\Bin\Release\SvtAv1Enc.lib -Destination .\;    # TAKEOUT SVTAV1ENC.LIB TO PATH
-        Remove-Item "~\AppData\Local\Microsoft\WindowsApps\SVT-AV1" -Recurse -Force;                                # REMOVE SVT-AV1 GIT CLONE
+        cargo install ab-av1;                                                             # INSTALL AB-AV1 VIA CARGO
 
-        cargo install ab-av1; # INSTALL AB-AV1 VIA CARGO
-
-        Read-Host -Prompt "`n`nINSTALLATION DONE! Launch by typing [ab-av1] directly.`nFor more info on how to use, do refer to this doc [https://alexheretic.github.io/posts/ab-av1/].`n`nThank you for using my program! Your support is very valuable for me.`n`nPress [ENTER] to exit installer...";
+        Read-Host -Prompt "`n`nINSTALLATION DONE! Launch by typing [ab-av1] directly.`nTo start a wizard, enter in [iwr https://github.com/HaiziIzzudin/Code_Scraps/raw/main/abav1_configurados.ps1 | iex] on a new terminal environment.`n`nThank you for using my program! Your support is very valuable for me.`n`nPress [ENTER] to exit installer...";
     }
 }
 else { # IF PLATFORM = LINUX
