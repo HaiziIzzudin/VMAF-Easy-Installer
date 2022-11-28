d4c4bed26be904d7bc000b6d963d3ee38b278ec3
Haizi? <79714350+HaiziIzzudin@users.noreply.github.com>
2022-11-28 22:47:14 +0800

    first initial release

--- vmaf-installer-linux.sh
+++ vmaf-installer-linux.sh
@@ -1,1 +1,8 @@
-
+sudo apt install software-properties-common -y;  # NOT INSTALLED BY DEFAULT IN DEBIAN, INSTALLED BY DEFAULT IN UBUNTU
+sudo add-apt-repository ppa:savoury1/ffmpeg4 -y; # ADDING PPA:SAVOURY FFMPEG 4
+sudo add-apt-repository ppa:savoury1/ffmpeg5 -y; # ADDING PPA:SAVOURY FFMPEG 5
+curl https://sh.rustup.rs -sSf | sh;             # INSTALL RUSTUP CARGO
+sudo apt update; 
+sudo apt upgrade -y; 
+sudo apt install ffmpeg -y;
+cargo install ab-av1;
