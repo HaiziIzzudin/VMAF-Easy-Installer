fe8cb85999735c1e7033042df75c21e418f78c51
Nama diberi Haizi <79714350+HaiziIzzudin@users.noreply.github.com>
2022-12-26 21:06:01 +0800

    asasasasaas

--- vmafeasy-v3.ps1
+++ vmafeasy-v3.ps1
@@ -4,6 +4,4 @@ Set-Location ~\AppData\Local\Microsoft\WindowsApps; # CD TO PATH
 
 if ((Test-Path -Path ".\ab-av1.exe") -eq $True) { Remove-Item ".\ab-av1.exe" -Recurse -Force }
 
-Invoke-WebRequest -Uri https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip -OutFile .\ffmpeg.zip;
-Expand-Archive -LiteralPath ".\ffmpeg.zip" -DestinationPath ".\";                                                       # DOWNLOAD & EXTRACT FFMPEG
-Invoke-WebRequest -Uri https://github.com/alexheretic/ab-av1/releases/download/v0.5.0/ab-av1.exe -OutFile .\ab-av1.exe  # DOWNLOAD AB-AV1.EXE FROM RELEASES
+Invoke-WebRequest -Uri https://github.com/alexheretic/ab-av1/releases/download/v0.6.1/ab-av1.exe -OutFile .\ab-av1.exe  # DOWNLOAD AB-AV1.EXE FROM RELEASES
